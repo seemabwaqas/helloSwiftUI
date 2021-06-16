d7de362615b69d1487116988de8cd067a15e1b55
Hafiza <hafiza@ezelink.com>
2021-06-16 16:55:46 +0400

    Initial Commit

---
+++ Hello SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello SwiftUI
+//
+//  Created by Hafiza Seemab on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
